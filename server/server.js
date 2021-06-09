04355c60329942117a91b54b7c36f2c00b00d3a9
Danilo Bodelon <danilobodelon@hotmail.com>
2021-06-09 10:48:15 -0300

    First commit

---
+++ server/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const database = require('./config/database')
+
+//Método do express json(). This middleware is available in Express v4.16.0 onwards. headers na requisição: {"Content-Type": "application/json"}
+app.use(express.json());
+
+app.get('/produtos', async function(req, res) {     
+    await database.query('select * from produtos',
+        function (err, results) {   
+            if (err) throw err;         
+            res.json(results)
+        }
+    )        
+});
+
+app.get('/produtos/:id', async function(req, res) {  
+    const id = req.params.id
+    await database.query('select * from produtos where id = ?', id,
+        function (err, results) {
+            if (err) throw err;            
+            res.json(results)
+        }
+    )        
+});
+
+app.post('/produtos/', async function(req, res) {
+    const post = req.body;
+    await database.query('INSERT INTO produtos (nome, descricao, preco) values ( ?, ?, ? )', [post.nome, post.descricao, post.preco],
+        function (err, results) { 
+            if (err) throw err;
+            res.json(results.affectedRows)                       
+        }
+    )        
+});
+
+app.put('/produtos/:id', async function(req, res){
+    const id = req.params.id
+    const post = req.body; 
+    await database.query('UPDATE produtos SET nome = ?, descricao = ?, preco = ? WHERE id = ?', [post.nome, post.descricao, post.preco, id], 
+    function(err, results){
+        if (err) throw err;
+        res.json(results.affectedRows) 
+    })
+})
+
+app.delete('/produtos/:id', async function(req, res){
+    const id = req.params.id 
+    await database.query('DELETE FROM produtos WHERE id = ?', id, function(err, results){
+        if (err) throw err;
+        res.json(results.affectedRows) 
+    })
+})
+
+app.listen(3000)
+
